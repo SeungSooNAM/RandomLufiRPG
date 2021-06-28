bd77d35b180f763a7a8b92cfee5904e4426768b4
Kimeunho <ehk1248@gmail.com>
2021-06-28 23:55:29 +0900

    김은호02

--- src/com/npng/onepiece/run/Application.java
+++ src/com/npng/onepiece/run/Application.java
@@ -1,10 +1,12 @@
 package com.npng.onepiece.run;
 
+import com.npng.onepiece.user.view.LoginView;
+
 public class Application {
 
 	public static void main(String[] args) {
 		
-
+		new LoginView();
 		
 	}
 }
