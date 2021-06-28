53d4a5e2b6f0cf0a8d2ae2221067c509bfeac191
82106 <82106@DESKTOP-16P3EMG>
2021-06-29 00:16:22 +0900

    ㅁ

--- src/com/npng/onepiece/event/controller/eventController.java
+++ src/com/npng/onepiece/event/controller/eventController.java
@@ -1,5 +1,41 @@
 package com.npng.onepiece.event.controller;
 
+import javax.swing.JLabel;
+
+import com.npng.onepiece.event.model.dto.SceDTO;
+import com.npng.onepiece.event.model.eventService.EventService;
+import com.npng.onepiece.event.views.EventSceView;
+
 public class eventController {
-   
+	
+	private EventService eventService = new EventService();
+	
+	public void random() {
+		
+		int number = (int)(Math.random() *3)+1;
+		if(number == 1) { // 전투
+			
+		} if(number >= 2) {  // 일반
+			
+		}
+	}
+	
+	public JLabel scenum(int num) {
+		
+		String story = EventService.scenum(num);
+		  
+		return story;
+//		EventSceView view = new EventSceView();
+//		view.EventSceView(stroy);
+	;
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
 }
---
+++ src/com/npng/onepiece/event/model/dao/SceDAO.java
@@ -0,0 +1,65 @@
+package com.npng.onepiece.event.model.dao;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static com.npng.onepiece.common.JDBCTemplate.close;
+
+import com.npng.onepiece.event.model.dto.SceDTO;
+
+public class SceDAO {
+	
+	private static Properties prop;
+	
+	public SceDAO() {
+		this.prop = new Properties();
+		try {
+			prop.loadFromXML(new FileInputStream("mapper/member-query.xml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String scenum(Connection con, int num) {
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		SceDTO story = null;
+		
+		String query = prop.getProperty("searhscenum");
+		
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setInt(1, num);
+			
+			rset = pstmt.executeQuery();
+			
+			while(rset.next()) {
+				
+				story = new SceDTO();
+				
+				story.setStroy(rset.getString("SCE_STORY"));
+				
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+			close(rset);
+
+		}
+		
+		
+		
+		return story;
+	}
+
+}
