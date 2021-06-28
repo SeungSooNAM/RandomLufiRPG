c52b36a425a1d47b370ebc85387d26f6b617a057
남승수 <@DESKTOP-J01NKGB>
2021-06-28 16:16:48 +0900

    패키지 생성

--- src/com/npng/onepiece/Application.java
+++ src/com/npng/onepiece/Application.java
@@ -5,6 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 
 		System.out.println("프로그램 실행");
+		System.out.println("해둠");
 	}
 
 }
---
+++ src/com/npng/onepiece/setting/package-info.java
@@ -0,0 +1,1 @@
+package com.npng.onepiece.setting;
