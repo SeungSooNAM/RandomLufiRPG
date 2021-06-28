c5bddeee3e12f1cd0e6e69d4f163a4478388b156
남승수 <@DESKTOP-J01NKGB>
2021-06-28 20:26:34 +0900

    수정

---
+++ src/com/npng/onepiece/gameready/model/dto/NewCharacterDTO.java
@@ -0,0 +1,10 @@
+package com.npng.onepiece.gameReady.model.dto;
+
+public class NewCharacterDTO {
+
+	private int num;
+	private String CharacterName;
+	private int Str;
+	private int Dex;
+	private int charisma;
+}
--- src/com/npng/onepiece/mainMenu/MainMenu.java
+++ src/com/npng/onepiece/mainMenu/MainMenu.java
@@ -8,24 +8,7 @@ import javax.swing.ImageIcon;
 import java.awt.Graphics;
 
 public class MainMenu extends JFrame{
-	private Image img;
 	
-	public MainMenu() {			
-		homeframe();
-	}
-	public void homeframe() {
-		
-		this.img = img;
-		this.setBounds(300, 150, 1400, 800);
-		setSize(1400,800);
-		setResizable(false);
-		setLocationRelativeTo(null);
-		setLayout(null);
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JPanel background = new JPanel();
-		
-		
-		
-	}
+	
+	
 }
