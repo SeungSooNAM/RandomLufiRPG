67580703c8968311866592ca515c98b50f8f6725
남승수 <@DESKTOP-J01NKGB>
2021-06-28 20:36:56 +0900

    메인메뉴 수정

--- src/com/npng/onepiece/mainMenu/MainMenu.java
+++ src/com/npng/onepiece/mainMenu/MainMenu.java
@@ -9,6 +9,29 @@ import java.awt.Graphics;
 
 public class MainMenu extends JFrame{
 	
+	private ImageIcon background =  new ImageIcon("image/mainMenu/mainBackground.PNG");
+	Image img = background.getImage();
 	
+	
+	public MainMenu() {
+		homeframe();
+	}
+	
+	public void homeframe() {
+		
+		setSize(1400, 800);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setLayout(null);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	public void paint(Graphics g) {
+		
+		g.drawImage(img, 0 , 0, getWidth(), getHeight(),this);
+	}
+	public static void main(String[] args){
+		new MainMenu();
+	}
 	
 }
